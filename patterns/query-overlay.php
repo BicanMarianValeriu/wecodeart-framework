bdd8d6e2cb83d9abe5699e7897699766f6514032
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2022-07-31 04:59:59 +0300

    v5.6.6

---
+++ patterns/query-overlay.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Query (Overlay)
+ * Slug: wecodeart/query-overlay
+ * Categories: wecodeart, query
+ * Block Types: core/query
+ */
+?>
+<!-- wp:query {"queryId":1,"query":{"perPage":0,"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"displayLayout":{"type":"flex","columns":3}} -->
+<div class="wp-block-query">
+    <!-- wp:query-title {"type":"archive","className":"my-5"} /-->
+    <!-- wp:term-description /-->
+    <!-- wp:post-template {"className":"my-5"} -->
+    <!-- wp:cover {"useFeaturedImage":true,"dimRatio":70,"overlayColor":"primary","contentPosition":"bottom center","className":"rounded shadow-lg"} -->
+    <div class="wp-block-cover has-custom-content-position is-position-bottom-center rounded shadow-lg">
+        <span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-70 has-background-dim"></span>
+        <div class="wp-block-cover__inner-container">
+            <!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+            <div class="wp-block-group has-white-color has-text-color has-link-color">
+            <!-- wp:post-title {"level":3,"isLink":true} /-->
+            <!-- wp:pattern {"slug":"wecodeart/entry-meta"} /-->
+            <!-- wp:post-excerpt {"moreText":""} /-->
+            <!-- wp:read-more {"style":{"spacing":{"padding":{"top":"10px","right":"25px","bottom":"10px","left":"25px"}},"border":{"width":"1px","radius":"100px"},"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"borderColor":"light","backgroundColor":"white","textColor":"dark","fontSize":"small"} /-->            </div>
+            <!-- /wp:group -->
+        </div>
+    </div>
+    <!-- /wp:cover -->
+    <!-- /wp:post-template -->
+    <!-- wp:query-pagination {"className":"mb-5"} -->
+    <!-- wp:query-pagination-numbers /-->
+    <!-- /wp:query-pagination -->
+    <!-- wp:query-no-results -->
+    <!-- wp:paragraph {"className":"mb-5","placeholder":"No results."} -->
+    <p class="mb-5">No results.</p>
+    <!-- /wp:paragraph -->
+    <!-- /wp:query-no-results -->
+</div>
+<!-- /wp:query -->
