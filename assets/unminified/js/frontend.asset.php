158143d7ee323da059be97773335a0f7ac12cc6f
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2023-02-06 15:55:41 +0200

    CSS reset

--- assets/unminified/js/frontend.asset.php
+++ assets/unminified/js/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '596de48cc1ed8eab6f37');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '1799d8ba754f42ae62c8');
