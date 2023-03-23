5dde8ed8f9a13b4c21125285d9609262aeb78ea6
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2023-03-23 17:33:11 +0200

    Translation updates

--- patterns/section-pricing.php
+++ patterns/section-pricing.php
@@ -12,7 +12,7 @@
     
         printf(
             esc_html__( 'Choose %s', 'wecodeart' ), 
-            sprintf( '<strong><span class="has-underline">%s</span></strong>', esc_html__( 'your plan', 'wecodeart' ) )
+            sprintf( '<strong><span class="has-underline">%s</span></strong>', esc_html__( 'Your Plan', 'wecodeart' ) )
         );
         
     ?></h2>
