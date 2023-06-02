f9bb95e2807dae5eabd190417e764bfe2004b5a7
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2023-06-02 23:20:14 +0300

    v6.1.2

--- patterns/section-woo.php
+++ patterns/section-woo.php
@@ -7,6 +7,20 @@
 ?>
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xxl","right":"var:preset|spacing|g","left":"var:preset|spacing|g"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","textColor":"white","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-white-color has-primary-background-color has-text-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--xxl);padding-right:var(--wp--preset--spacing--g);padding-left:var(--wp--preset--spacing--g)">
+    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+    <div class="wp-block-group">
+        <!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"top":"0.25rem","right":"0.25rem","bottom":"0.25rem","left":"0.25rem"}},"typography":{"lineHeight":"1"}},"backgroundColor":"accent","className":"overflow-hidden rounded-pill w-auto","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+            <div class="wp-block-group overflow-hidden rounded-pill w-auto has-accent-background-color has-background" style="padding-top:0.25rem;padding-right:0.25rem;padding-bottom:0.25rem;padding-left:0.25rem;line-height:1">
+            <!-- wp:paragraph {"style":{"typography":{"fontSize":"0.6rem"},"layout":{"selfStretch":"fill"},"spacing":{"padding":{"right":"0.45rem","bottom":"0.25rem","left":"0.45rem","top":"0.25rem"}}},"backgroundColor":"primary","textColor":"white","className":"fw-700 text-uppercase rounded-pill"} -->
+            <p class="fw-700 text-uppercase rounded-pill has-white-color has-primary-background-color has-text-color has-background" style="padding-top:0.25rem;padding-right:0.45rem;padding-bottom:0.25rem;padding-left:0.45rem;font-size:0.6rem">New</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph {"style":{"spacing":{"margin":{"right":"var:preset|spacing|g","left":"var:preset|spacing|g"}}},"textColor":"dark","fontSize":"small"} -->
+            <p class="has-dark-color has-text-color has-small-font-size" style="margin-right:var(--wp--preset--spacing--g);margin-left:var(--wp--preset--spacing--g)">Blockified</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
     <!-- wp:heading {"textAlign":"center"} -->
     <h2 class="wp-block-heading has-text-align-center"><strong>WooCommerce</strong> <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-white-color has-shadows-into-light-font-family"><?php esc_html_e( 'Ready', 'wecodeart' ); ?></mark></h2>
     <!-- /wp:heading -->
